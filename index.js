af959d70a655f526468798fc8dcdab391a150e57
Emilia Burza <emila.burza@gmail.com>
2025-02-08 13:03:14 +0100

    Create new course route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const generes = require('./generes')
+const validate = require('./validate')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 //define route
 app.get('/', (req, res) => {
@@ -11,6 +13,23 @@ app.get('/', (req, res) => {
 app.get('/api/generes', (req, res) => {
     res.send(generes)
     res.end()
+})
+
+app.get('/api/generes/:id', (req, res) => {
+    const genere = generes.find( el => el.name === parseInt(req.params.name))
+    if (!genere) return res.status(404).send('Genere with that name was not found')
+    
+    res.send(genere)
+})
+
+app.post('/api/generes', (req, res) => {
+    const genere = {
+        id: generes.length + 1,
+        name: req.body.name
+    }
+
+    generes.push(genere)
+    res.send(genere)
 })
 
 //listen on a port
